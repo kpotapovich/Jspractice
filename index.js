08bf67a3418ba2d16b5e0ff044af12eaf17e3128
kpotapovich <kris.potapovich@gmail.com>
2021-02-06 13:25:29 +0300

    меню с js

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-let photoMenuOpened = false
+const CLOSED_IMG = "https://img.icons8.com/material-outlined/24/000000/send-letter.png";
+const OPENED_IMG = "https://img.icons8.com/ios-glyphs/30/000000/down2.png";
 
-function photoMenuOnClick() {
-    if (photoMenuOpened) {
-        photoMenuOpened = false
-        close()
-    } else {
-        photoMenuOpened = true
-        open()
-    }
-}
-
-
-function open() {
-    let photoMenu = document.getElementById("photoMenu");
-    photoMenu.src = "https://img.icons8.com/ios-glyphs/30/000000/down2.png";
-    let photoList = document.getElementById("photo_list");
-    photoList.classList.remove("submenu-none");
-}
-
-function close() {
-    let photoMenu = document.getElementById("photoMenu");
-    photoMenu.src = "https://img.icons8.com/material-outlined/24/000000/send-letter.png";
-
-    let photoList = document.getElementById("photo_list");
-    photoList.classList.add("submenu-none");
+let menuStatus = {
+    photoMenu: false,
+    homeMenu: false
 }
 
 let photoMenu = document.getElementById("photoMenu");
-
+let homeMenu = document.getElementById("homeMenu");
 photoMenu.onclick = photoMenuOnClick;
+homeMenu.onclick = homeMenuOnClick;
+
+function photoMenuOnClick() {
+    menuStatus.photoMenu = !menuStatus.photoMenu;
+    photoMenu.src = menuStatus.photoMenu ? OPENED_IMG : CLOSED_IMG;
+    let photoList = document.getElementById("photo_list");
+    photoList.classList.toggle("submenu-none")
+}
+
+function homeMenuOnClick() {
+    menuStatus.homeMenu = !menuStatus.homeMenu;
+    homeMenu.src = menuStatus.homeMenu ? OPENED_IMG : CLOSED_IMG;
+    let homeList = document.getElementById("home_list");
+    homeList.classList.toggle("submenu-none");
+}
